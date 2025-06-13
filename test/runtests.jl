022ddc5f8a348386f516c93b387bcd5b9ef16e49
Uwe Fechner <fechner@aenarete.eu>
2025-06-13 16:50:02 +0200

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using FLORIDyn
 using Test
 
+# Suppose WindDir is a matrix where each row is [time, phi_T0, phi_T1, ...]
+WindDir = [
+    0.0  10.0  20.0
+    1.0  12.0  22.0
+    2.0  14.0  24.0
+]
+
 @testset "FLORIDyn.jl" begin
-    # Write your tests here.
+    phi = getWindDirT_EnKF(WindDir, 1, 0.5)
+    @test phi ≈ 11.0
 end
