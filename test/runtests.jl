3eab88ae212fc8f172f2b18fee25db91135eefd0
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-06-17 13:51:48 +0200

    add random number from file

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,45 @@
 using FLORIDyn
 using Test
 using LinearAlgebra
-using Random
 using MAT
+using Random
 
-Random.seed!(1234)
+if basename(pwd()) == "test"
+    cd("..")
+end
+
 filename="test/randn.mat"
 randn_vec=vec(matread(filename)["vec"])
 
-include("test_windfield.jl")
+# const DNG = Iterators.Stateful(randn_vec)
+# randn() = popfirst!(DNG)
+# randn(n) = collect(Iterators.take(DNG, n))
+
+mutable struct FileRNG <: AbstractRNG
+    const data::Vector{Float64}
+    idx::Int
+end
+
+FileRNG(data::Vector{Float64}) = FileRNG(data, 1)
+
+function Base.rand(rng::FileRNG)
+    x = rng.data[rng.idx]
+    rng.idx += 1
+    return x
+end
+
+function Base.rand(rng::FileRNG, ::Type{Float64})
+    return rand(rng)
+end
+
+function Base.rand(rng::FileRNG, ::Type{T}) where {T}
+    error("FileRNG only supports Float64")
+end
+
+# Optionally, support randn
+Random.randn(rng::FileRNG) = rand(rng)
+
+rng = FileRNG(randn_vec)
+
+
+# include("test_windfield.jl")
