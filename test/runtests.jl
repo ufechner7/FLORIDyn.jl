a90ed40fd2ca7c10bb371b20b4b11c5d662ed0cf
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-06-17 11:16:22 +0200

    Read randn.mat file for testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,10 @@ using FLORIDyn
 using Test
 using LinearAlgebra
 using Random
+using MAT
 
 Random.seed!(1234)
+filename="test/randn.mat"
+randn_vec=vec(matread(filename)["vec"])
 
 include("test_windfield.jl")
