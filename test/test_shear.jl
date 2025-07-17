75dc2b4fec576060884544c55cbb6fa36d6a39d6
Uwe Fechner <fechner@aenarete.eu>
2025-07-17 08:38:02 +0200

    Improve formatting of test results

--- test/test_shear.jl
+++ test/test_shear.jl
@@ -6,15 +6,17 @@ using Test
 using LinearAlgebra
 using Random
 
-@testset "wind shear     " begin
-    shear_mode = Shear_Interpolation()
-    wind_shear = [10 0.8; 20 0.9; 30 1.0]  # Example data
-    z = [5, 15, 25, 35]                   # Heights to interpolate at
-    shear = getWindShearT(shear_mode, wind_shear, z)
-    @test shear[1] ≈ 0.8
-    @test shear[2] ≈ 0.85
-    @test shear[3] ≈ 0.95
-    @test shear[4] ≈ 1.0
+@testset verbose=true "wind shear                                              " begin
+    @testset "getWindShearT(Shear_Interpolation(), ...)" begin
+        shear_mode = Shear_Interpolation()
+        wind_shear = [10 0.8; 20 0.9; 30 1.0]  # Example data
+        z = [5, 15, 25, 35]                   # Heights to interpolate at
+        shear = getWindShearT(shear_mode, wind_shear, z)
+        @test shear[1] ≈ 0.8
+        @test shear[2] ≈ 0.85
+        @test shear[3] ≈ 0.95
+        @test shear[4] ≈ 1.0
+    end
     @testset "getWindShearT(Shear_PowerLaw(), ...)" begin
         shear_mode = Shear_PowerLaw()
         # Test with scalar z_norm
