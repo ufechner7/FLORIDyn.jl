0ef5fe286285032dbe7f3c324bf1bc6b84624633
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-17 15:16:56 +0200

    Update main.jl

--- examples/main.jl
+++ examples/main.jl
@@ -2,7 +2,9 @@
 # Improved FLORIDyn approach over the gaussian FLORIDyn model
 # Link folder with simulation data
 
-using FLORIDyn
+using FLORIDyn, YAML
 
-pathToSimulation = "2021_9T_Data"
-addPaths()
+YAML.load_file("data/2021_9T_Data.yaml")
+
+# Get the settings for the wind field, visualization, controller and Sim.
+# Wind, Vis, Sim, Con = setup("2021_9T_Data")
