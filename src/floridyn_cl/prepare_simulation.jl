d8b3074462c146f58be296fcfa376ff43cc3e2d7
Uwe Fechner <fechner@aenarete.eu>
2025-07-20 12:59:32 +0200

    Update comment

--- src/floridyn_cl/prepare_simulation.jl
+++ src/floridyn_cl/prepare_simulation.jl
@@ -212,8 +212,8 @@ function prepareSimulation(set::Settings, wind, con, paramFLORIDyn, paramFLORIS,
     # end
 
     # # ========== Init State ===========
-    #TODO fix InitStates() 
-    T[:States_OP], T[:States_T], T[:States_WF] = InitStates(set, T, wind, turbProp.Init_States, paramFLORIS, sim)
+    #TODO implement Centerline
+    #T[:States_OP], T[:States_T], T[:States_WF] = InitStates(set, T, wind, turbProp.Init_States, paramFLORIS, sim)
 
     # # ========== Simulation Setup ==========
     # sim.nSimSteps = length(sim.start_time:sim.TimeStep:sim.end_time)
