a5b32ad30cefd6bfe6ece35b085c4eac9c8225ae
Uwe Fechner <fechner@aenarete.eu>
2025-07-21 12:03:26 +0200

    Comment untranslated code

--- src/floridyn_cl/floridyn_cl.jl
+++ src/floridyn_cl/floridyn_cl.jl
@@ -50,43 +50,45 @@ function FLORIDynCL(set::Settings, T, Wind, Sim, Con, paramFLORIDyn, paramFLORIS
         # ========== PREDICTION ==========
         T = iterateOPs!(set.iterate_mode, T, Sim, paramFLORIS, paramFLORIDyn)
 
-        # ========== Wind Field Perturbation ==========
-        T = pertubationOfTheWF(T, Wind)
+    #     # ========== Wind Field Perturbation ==========
+    #     T = pertubationOfTheWF(T, Wind)
 
-        # ========== Get FLORIS reductions ==========
-        T.dep = findTurbineGroups(T, paramFLORIDyn)
-        T.intOPs = interpolateOPs(T)
-        tmpM, T = setUpTmpWFAndRun(T, paramFLORIS, Wind)
-        M[(it-1)*nT+1 : it*nT, 2:4] = tmpM
-        M[(it-1)*nT+1 : it*nT, 1] = SimTime
-        T.States_T[T.StartI, 3] = tmpM[:, 2]
-        M_int[it] = T.red_arr
+    #     # ========== Get FLORIS reductions ==========
+    #     T.dep = findTurbineGroups(T, paramFLORIDyn)
+    #     T.intOPs = interpolateOPs(T)
+    #     tmpM, T = setUpTmpWFAndRun(T, paramFLORIS, Wind)
+    #     M[(it-1)*nT+1 : it*nT, 2:4] = tmpM
+    #     M[(it-1)*nT+1 : it*nT, 1] = SimTime
+    #     T.States_T[T.StartI, 3] = tmpM[:, 2]
+    #     M_int[it] = T.red_arr
 
-        # ========== Wind field corrections ==========
-        T, Wind = correctVel(T, Wind, SimTime, paramFLORIS, tmpM)
-        T = correctDir(T, Wind, SimTime)
-        T = correctTi(T, Wind, SimTime)
+    #     # ========== Wind field corrections ==========
+    #     T, Wind = correctVel(T, Wind, SimTime, paramFLORIS, tmpM)
+    #     T = correctDir(T, Wind, SimTime)
+    #     T = correctTi(T, Wind, SimTime)
 
-        # Save free wind speed as measurement
-        M[(it-1)*nT+1 : it*nT, 5] = T.States_WF[T.StartI, 1]
+    #     # Save free wind speed as measurement
+    #     M[(it-1)*nT+1 : it*nT, 5] = T.States_WF[T.StartI, 1]
 
-        # ========== Get Control settings ==========
-        T.States_T[T.StartI, 2] = (
-            T.States_WF[T.StartI, 2] .-
-            getYaw(Con.YawData, collect(1:nT), SimTime)'
-        )
+    #     # ========== Get Control settings ==========
+    #     T.States_T[T.StartI, 2] = (
+    #         T.States_WF[T.StartI, 2] .-
+    #         getYaw(Con.YawData, collect(1:nT), SimTime)'
+    #     )
 
-        # ========== Calculate Power ==========
-        P = getPower(T, tmpM, paramFLORIS, Con)
-        M[(it-1)*nT+1:it*nT, 6] = P
+    #     # ========== Calculate Power ==========
+    #     P = getPower(T, tmpM, paramFLORIS, Con)
+    #     M[(it-1)*nT+1:it*nT, 6] = P
 
-        SimTime += Sim.TimeStep
+        SimTime += Sim.time_step
     end
-    # Convert `M` to DataFrame and scale measurements
-    Mt = DataFrame(
-        (M .* [1; 100; 100; 1; 1; 1e-6])',
-        [:Time, :ForeignReduction, :AddedTurbulence, :EffWindSpeed, :FreeWindSpeed, :PowerGen]
-    )
-    Mint = hcat(Mt.Time, hcat(M_int...)')
+    # # Convert `M` to DataFrame and scale measurements
+    # Mt = DataFrame(
+    #     (M .* [1; 100; 100; 1; 1; 1e-6])',
+    #     [:Time, :ForeignReduction, :AddedTurbulence, :EffWindSpeed, :FreeWindSpeed, :PowerGen]
+    # )
+    # Mint = hcat(Mt.Time, hcat(M_int...)')
+    Mt = nothing
+    Mint = nothing
     return T, Mt, Mint
 end
