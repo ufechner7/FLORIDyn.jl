4ace71e51694d5112fff3874b30a6a9a5cb44836
Uwe Fechner <fechner@aenarete.eu>
2025-07-21 12:00:26 +0200

    iterateOPs! works

--- src/floridyn_cl/iterate.jl
+++ src/floridyn_cl/iterate.jl
@@ -13,39 +13,41 @@ function iterateOPs!(::IterateOPs_basic, T, Sim, paramFLORIS, paramFLORIDyn)
     T[:States_OP][:, 4] .+= step_dw
 
     # Crosswind step
-    deflection = Centerline(T.States_OP, T.States_T, T.States_WF, paramFLORIS, T.D[1])
-    step_cw = deflection .- T.States_OP[:, 5:6]
-    T.States_OP[:, 5:6] .= deflection
+    deflection = Centerline(T[:States_OP], T[:States_T], T[:States_WF], paramFLORIS, T[:D][1])
+    step_cw = deflection .- T[:States_OP][:, 5:6]
+    T[:States_OP][:, 5:6] .= deflection
 
     # World coordinate system adjustment
-    phiW = angSOWFA2world(T.States_WF[:, 2])
-    T.States_OP[:, 1] .+= cos.(phiW) .* step_dw .- sin.(phiW) .* step_cw[:, 1]
-    T.States_OP[:, 2] .+= sin.(phiW) .* step_dw .+ cos.(phiW) .* step_cw[:, 1]
-    T.States_OP[:, 3] .+= step_cw[:, 2]
+    phiW = angSOWFA2world.(T[:States_WF][:, 2])
+    T[:States_OP][:, 1] .+= cos.(phiW) .* step_dw .- sin.(phiW) .* step_cw[:, 1]
+    T[:States_OP][:, 2] .+= sin.(phiW) .* step_dw .+ cos.(phiW) .* step_cw[:, 1]
+    T[:States_OP][:, 3] .+= step_cw[:, 2]
 
     # Circshift & init first OPs
     # OPs
-    T.States_OP = circshift(T.States_OP, 1; dims=1)
-    T.States_OP[T.StartI, :] = tmpOPStates
+    T[:States_OP] = circshift(T[:States_OP], (1, 0))
+    # T[:States_OP] = circshift(T[:States_OP], (1, 0))
+    T[:States_OP][T[:StartI], :] = tmpOPStates
 
     # Turbines
-    T.States_T = circshift(T.States_T, 1; dims=1)
-    T.States_T[T.StartI, :] = tmpTStates
+    T[:States_T] = circshift(T[:States_T], (1, 0))
+    T[:States_T][T[:StartI], :] = tmpTStates
 
     # Wind Farm
-    T.States_WF = circshift(T.States_WF, 1; dims=1)
-    T.States_WF[T.StartI, :] = tmpWFSTates
+    T[:States_WF] = circshift(T[:States_WF], (1, 0))
+    T[:States_WF][T[:StartI], :] = tmpWFSTates
 
     # Check if OPs are in order
-    for iT in 1:T.nT
-        inds = T.StartI[iT]:(T.StartI[iT] + T.nOP - 1)
-        _, indOP = sort(T.States_OP[inds, 4], dims=1)
-        if !issorted(indOP)
-            # Sort rows according to the new order
-            T.States_OP[inds, :] = T.States_OP[inds[indOP], :]
-            T.States_T[inds, :]  = T.States_T[inds[indOP], :]
-            T.States_WF[inds, :] = T.States_WF[inds[indOP], :]
+    for iT in 1:T[:nT]
+        inds = T[:StartI][iT]:(T[:StartI][iT] + T[:nOP] - 1)
+
+        indOP = sortperm(T[:States_OP][inds, 4])
+        if indOP != sort(indOP)  # check if already sorted
+            T[:States_OP][inds, :] = T[:States_OP][inds[indOP], :]
+            T[:States_T][inds, :]  = T[:States_T][inds[indOP], :]
+            T[:States_WF][inds, :] = T[:States_WF][inds[indOP], :]
         end
+
     end
     return T
 end
