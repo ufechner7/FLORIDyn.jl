6acd2a372ed06c36dbca0807a7909cc0d026a842
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-22 03:37:29 +0200

    Cleanup

--- examples/main.jl
+++ examples/main.jl
@@ -3,7 +3,7 @@
 
 # MainFLORIDyn Center-Line model
 # Improved FLORIDyn approach over the gaussian FLORIDyn model
-using FLORIDyn, YAML
+using FLORIDyn
 
 settings_file = "data/2021_9T_Data.yaml"
 
@@ -15,14 +15,12 @@ set = Settings(wind, sim, con)
 
 # % Load linked data
 turbProp        = turbineArrayProperties(settings_file)
-paramFLORIS     = floris
-paramFLORIDyn   = floridyn
 
-T, wind, sim, con, paramFLORIS = prepareSimulation(set, wind, con, paramFLORIDyn, paramFLORIS, turbProp, sim)
+T, wind, sim, con, paramFLORIS = prepareSimulation(set, wind, con, floridyn, floris, turbProp, sim)
 
 # Run initial conditions until no more change happens (wrong comment in original code)
-T = initSimulation(T, wind, sim, con, paramFLORIDyn, paramFLORIS)
+T = initSimulation(T, wind, sim, con, floridyn, floris)
 
-@time T, M, Mint = FLORIDynCL(set, T, wind, sim, con, paramFLORIDyn, paramFLORIS)
+@time T, M, Mint = FLORIDynCL(set, T, wind, sim, con, floridyn, floris)
 # 0.23 s on Desktop, 0.40 s with MATLAB
 nothing
