556a9d98364af346894548efd0bb27da67de6b2f
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-25 21:56:09 +0200

    Add benchmark

---
+++ test/bench_setup.jl
@@ -0,0 +1,22 @@
+# Copyright (c) 2025 Uwe Fechner
+# SPDX-License-Identifier: BSD-3-Clause
+
+using FLORIDyn, Test, BenchmarkTools
+
+settings_file = "data/2021_9T_Data.yaml"
+# get the settings for the wind field, simulator and controller
+wind, sim, con, floris, floridyn = setup(settings_file)
+# create settings struct
+set = Settings(wind, sim, con)
+# % Load linked data
+turbine_prop        = turbineArrayProperties(settings_file)
+wf, wind, sim, con, floris = prepareSimulation(set, wind, con, floridyn, floris, turbine_prop, sim)
+wf.dep = findTurbineGroups(wf, floridyn)
+wf.intOPs = interpolateOPs(wf)
+wf_old = deepcopy(wf)
+@btime setUpTmpWFAndRun(set, wf, floris, wind)
+nothing
+
+# Laptop on battery
+# 116.358 μs (2357 allocations: 397.69 KiB)
+    
