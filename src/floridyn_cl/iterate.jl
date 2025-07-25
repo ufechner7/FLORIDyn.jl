f291a154a569d819c46aadfac1efab97c0ab1b96
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-25 23:17:34 +0200

    Use issorted, one more view

--- src/floridyn_cl/iterate.jl
+++ src/floridyn_cl/iterate.jl
@@ -124,8 +124,8 @@ function iterateOPs!(::IterateOPs_basic, wf::WindFarm, sim::Sim, floris::Floris,
     for iT in 1:wf.nT
         inds = wf.StartI[iT]:(wf.StartI[iT] + wf.nOP - 1)
 
-        indOP = sortperm(wf.States_OP[inds, 4])
-        if indOP != sort(indOP)  # check if already sorted
+        indOP = sortperm(@view wf.States_OP[inds, 4])
+        if ! issorted(indOP)  # check if already sorted
            wf.States_OP[inds, :] = wf.States_OP[inds[indOP], :]
            wf.States_T[inds, :]  = wf.States_T[inds[indOP], :]
            wf.States_WF[inds, :] = wf.States_WF[inds[indOP], :]
