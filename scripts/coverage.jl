9c61c9898472b195b09a03caf8709b79356b179c
Uwe Fechner <fechner@aenarete.eu>
2025-07-27 00:58:31 +0200

    Improve coverage.jl

--- scripts/coverage.jl
+++ scripts/coverage.jl
@@ -1,14 +1,32 @@
 # Copyright (c) 2025 Uwe Fechner
 # SPDX-License-Identifier: BSD-3-Clause
 
+using Pkg
+
+function globaldependencies()
+    projectpath = Pkg.project().path
+    basepath, _ = splitdir(projectpath)
+    Pkg.activate()
+    globaldependencies = keys(Pkg.project().dependencies)
+    Pkg.activate(basepath)
+    globaldependencies
+end
+
+if !("LiveServer" in globaldependencies())
+    println("Installing LiveServer globally!")
+    run(`julia -e 'using Pkg; Pkg.add("LiveServer")'`)
+end
+if !("LocalCoverage" in globaldependencies())
+    println("Installing LocalCoverage globally!")
+    run(`julia -e 'using Pkg; Pkg.add("LocalCoverage")'`)
+end
+
 using LocalCoverage
 import LiveServer as LS
 
-coverage = generate_coverage("FLORIDyn"; run_test = true)
-html_coverage(coverage; open = false, dir = tempdir())
-LS.serve(launch_browser=true, dir="/tmp")
+git_dir = pwd()
 
-@info "On Ubuntu 2024 you need to execute:"
-@info "cd /tmp"
-@info "python3 -m http.server 8000"
-@info "And then open http://localhost:8000 with your browser."
+html_dir = tempdir()
+coverage = generate_coverage("FLORIDyn"; run_test = true)
+html_coverage(coverage; gitroot=git_dir, open = false, dir = html_dir)
+LS.serve(launch_browser=true, dir=html_dir)
