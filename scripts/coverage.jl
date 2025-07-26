09c3ad743dfdda9d7de6b7140ddf9c13be618c3c
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-26 21:29:01 +0200

    Update coverage.jl

--- scripts/coverage.jl
+++ scripts/coverage.jl
@@ -2,3 +2,8 @@ using LocalCoverage
 
 coverage = generate_coverage("FLORIDyn"; run_test = true)
 html_coverage(coverage; open = true, dir = tempdir())
+
+@info "On Ubuntu 2024 you need to execute:"
+@info "cd /tmp"
+@info "python3 -m http.server 8000"
+@info "And then open http://localhost:8000 with your browser."
