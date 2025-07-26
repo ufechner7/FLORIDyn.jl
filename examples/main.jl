63faefe317371622b78ec75ab34e62d9c7736920
Uwe Fechner <fechner@aenarete.eu>
2025-07-26 13:46:47 +0200

    Add comments

--- examples/main.jl
+++ examples/main.jl
@@ -24,7 +24,8 @@ wf = initSimulation(wf, sim)
 @time wf, md, mi = runFLORIDyn(set, wf, wind, sim, con, floridyn, floris)
 # 0.115 s on Desktop, 0.39 s with MATLAB
 # 0.115 seconds (891.24 k allocations: 368.147 MiB, 10.18% gc time)
-# 0.110 seconds (883.14 k allocations: 272.819 MiB, 9.62% gc time)
+# 0.110 seconds (883.14 k allocations: 272.819 MiB, 9.62% gc time) iterateOPs! allocation free
+# 0.081 seconds (723.31 k allocations: 168.226 MiB, 8.10% gc time) findTurbineGroups allocation free
 
 @info "Type 'md |> pager' to see the results of the simulation."
 @info "Type 'q' to exit the pager."
