d6b3f63d0199e61519fc3785050f79be79487b43
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-29 11:04:04 +0200

    Fix #30

--- src/FLORIDyn.jl
+++ src/FLORIDyn.jl
@@ -249,10 +249,12 @@ include("visualisation/plot_flowfield.jl")
 @setup_workload begin
     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
     # precompile file and potentially make loading faster.
+    path = dirname(pathof(@__MODULE__))
+    path = (joinpath(path, "..", "data"))
     @compile_workload begin
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
-        settings_file = "data/2021_9T_Data.yaml"
+        settings_file = joinpath(path, "2021_9T_Data.yaml")
         wind, sim, con, floris, floridyn, ta = setup(settings_file)
         set = Settings(wind, sim, con)
         wf, wind, sim, con, floris = prepareSimulation(set, wind, con, floridyn, floris, ta, sim)
