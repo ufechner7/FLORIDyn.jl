2997947b5af2761deb68d5fbf1e9500a985f912a
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-08-01 21:24:52 +0200

    Add test_case_01.jl

---
+++ examples/test_case_01.jl
@@ -0,0 +1,45 @@
+# Copyright (c) 2025 Marcus Becker, Uwe Fechner
+# SPDX-License-Identifier: BSD-3-Clause
+
+# MainFLORIDyn Center-Line model
+# Improved FLORIDyn approach over the gaussian FLORIDyn model
+using Timers
+tic()
+using FLORIDyn, TerminalPager, ControlPlots
+
+settings_file = "data/2021_9T_Data.yaml"
+vis = Vis(online=false, save=true, rel_v_min=20.0, up_int = 4)
+PARALLEL = true
+
+function get_parameters(vis, parallel=PARALLEL)
+    # get the settings for the wind field, simulator and controller
+    wind, sim, con, floris, floridyn, ta = setup(settings_file)
+
+    # create settings struct
+    set = Settings(wind, sim, con)
+    set.parallel = parallel
+
+    wf, wind, sim, con, floris = prepareSimulation(set, wind, con, floridyn, floris, ta, sim)  
+    wf = initSimulation(wf, sim)
+    wf, md, mi = runFLORIDyn(nothing, set, wf, wind, sim, con, vis, floridyn, floris)
+    return wf, md, set, floris, wind 
+end
+
+# get the settings for the wind field, simulator and controller
+wind, sim, con, floris, floridyn, ta = setup(settings_file)
+
+# create settings struct
+set = Settings(wind, sim, con)
+set.parallel = PARALLEL
+
+wf, wind, sim, con, floris = prepareSimulation(set, wind, con, floridyn, floris, ta, sim)
+sim.n_sim_steps = 195
+
+# Run initial conditions until no more change happens (wrong comment in original code)
+wf = initSimulation(wf, sim)
+
+vis.online = false
+@time wf, md, mi = runFLORIDyn(plt, set, wf, wind, sim, con, vis, floridyn, floris)
+@time Z, X, Y = calcFlowField(set, wf, wind, floris; plt)
+plotFlowField(plt, wf, X, Y, Z, vis; msr=3)
+nothing
