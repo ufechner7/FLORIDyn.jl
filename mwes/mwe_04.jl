b7010ce94ee6c4825c6a04aae6f06f0ac1039312
Uwe Fechner <fechner@aenarete.eu>
2025-08-02 13:28:03 +0200

    Update mwe_04.jl

--- mwes/mwe_04.jl
+++ mwes/mwe_04.jl
@@ -5,20 +5,24 @@
 
 using Distributed, Timers
 
-# Only add a worker if we don't have any dedicated worker processes
-if nprocs() < 2  # nprocs() counts main + workers, so < 2 means no dedicated workers
-    println("No dedicated workers found, adding 1 worker...")
-    @time addprocs(1)
-else
-    println("Already have $(nprocs()-1) dedicated worker(s), total processes: $(nprocs())")
-    println("Workers: $(workers())")
+function init_worker()
+    # Only add a worker if we don't have any dedicated worker processes
+    if nprocs() < 2  # nprocs() counts main + workers, so < 2 means no dedicated workers
+        println("No dedicated workers found, adding 1 worker...")
+        @time addprocs(1)
+        @eval @everywhere using ControlPlots  # Ensure ControlPlots is available on all workers
+        @eval @everywhere using FLORIDyn      # Ensure FLORIDyn (including WindFarm) is available on all workers
+    else
+        println("Already have $(nprocs()-1) dedicated worker(s), total processes: $(nprocs())")
+        println("Workers: $(workers())")
+    end
 end
 
 tic()
-@everywhere using ControlPlots  # Ensure ControlPlots is available on all workers
-@everywhere using FLORIDyn      # Ensure FLORIDyn (including WindFarm) is available on all workers
 using ControlPlots
 using FLORIDyn
+toc()
+init_worker()
 toc()
 
 # Test that WindFarm is available on all workers
