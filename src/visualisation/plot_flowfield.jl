ca4e9e25df3194bd46388ffc908d8d6c2cb5a17c
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-08-02 07:38:06 +0200

    Fix comment

--- src/visualisation/plot_flowfield.jl
+++ src/visualisation/plot_flowfield.jl
@@ -99,7 +99,7 @@ end
 - This function requires a plotting package like ControlPlots.jl to be loaded and available as `plt`
 """
 function plotFlowField(state::Union{Nothing, PlotState}, plt, wf, mx, my, mz, vis, t=nothing; msr=3)
-    # Use unit_test from vis if not explicitly overridden
+    # Use unit_test from vis
     use_unit_test = vis.unit_test
     
     # Extract the 2D slice for the specified measurement
