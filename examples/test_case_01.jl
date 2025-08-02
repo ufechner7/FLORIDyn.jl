42db796f9d60033c9150e347014c73ef93b09a14
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-08-02 05:45:49 +0200

    Cleanup

--- examples/test_case_01.jl
+++ examples/test_case_01.jl
@@ -1,20 +1,18 @@
 # Copyright (c) 2025 Marcus Becker, Uwe Fechner
 # SPDX-License-Identifier: BSD-3-Clause
 
-# MainFLORIDyn Center-Line model
-# Improved FLORIDyn approach over the gaussian FLORIDyn model
-using Timers
-tic()
+# Testcase for bug https://github.com/ufechner7/FLORIDyn.jl/issues/35
 using FLORIDyn, TerminalPager, ControlPlots
 
 settings_file = "data/2021_9T_Data.yaml"
 vis = Vis(online=false, save=true, rel_v_min=20.0, up_int = 4)
+PARALLEL = false
 
 # get the settings for the wind field, simulator and controller
 wind, sim, con, floris, floridyn, ta = setup(settings_file)
 
 # create settings struct
-set = Settings(wind, sim, con)
+set = Settings(wind, sim, con, PARALLEL)
 
 wf, wind, sim, con, floris = prepareSimulation(set, wind, con, floridyn, floris, ta, sim)
 sim.n_sim_steps = 195
