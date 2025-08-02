298d1118ca3ac0c954355f2d718e502af32b52cc
Uwe Fechner <fechner@aenarete.eu>
2025-08-02 12:05:59 +0200

    Update mwe_03.jl

--- mwes/mwe_03.jl
+++ mwes/mwe_03.jl
@@ -1,11 +1,18 @@
-using DistributedNext
+# Copyright (c) 2025 Uwe Fechner
+# SPDX-License-Identifier: BSD-3-Clause
 
-# Only add a worker if we don't have any workers yet
-if nworkers() == 0
-    println("No workers found, adding 1 worker...")
-    addprocs(1)
+# Example of a simple Julia script to demonstrate the use of DistributedNext for parallel processing
+# This script initializes a worker, defines a function, and runs it on the worker
+
+using Distributed
+
+# Only add a worker if we don't have any dedicated worker processes
+if nprocs() < 2  # nprocs() counts main + workers, so < 2 means no dedicated workers
+    println("No dedicated workers found, adding 1 worker...")
+    @time addprocs(1)
 else
-    println("Already have $(nworkers()) worker(s), using existing workers")
+    println("Already have $(nprocs()-1) dedicated worker(s), total processes: $(nprocs())")
+    println("Workers: $(workers())")
 end
 
 # Define function on all processes (main + workers)
