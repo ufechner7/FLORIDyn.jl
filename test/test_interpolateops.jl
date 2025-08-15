496143535c0fc0007edb0e10bb04f2d0860d933a
Uwe Fechner <fechner@aenarete.eu>
2025-08-15 17:43:02 +0200

    Fix test

--- test/test_interpolateops.jl
+++ test/test_interpolateops.jl
@@ -24,14 +24,14 @@ vars_after_interpolateOPs_T = matread(after_interpolateOPs_T_file)
 wf_dict_02 = vars_after_interpolateOPs_T["T"]
 
 @testset "interpolateOPs_basic" begin
-    global wf, wf_ref_03
-    wf_ref_03 = convert_wf_dict2windfarm(wf_dict_03) # before_interpolateOPs_T
+    global wf, wf_ref_02
+    wf = convert_wf_dict2windfarm(wf_dict_03) # before_interpolateOPs_T
     wf.intOPs = interpolateOPs(wf)
-    
+
     wf_ref_02 = convert_wf_dict2windfarm(wf_dict_02) # after_interpolateOPs_T
-    if !compare_windFarms(wf_ref_02, wf_debug[1]; detailed=false, tolerance=1e-6)
+    if !compare_windFarms(wf_ref_02, wf; detailed=false, tolerance=1e-6)
         @warn "WindFarm does not match reference after interpolateOPs"
-        compare_windFarms(wf_ref_02, wf_debug[1]; detailed=true, tolerance=1e-6)
+        compare_windFarms(wf_ref_02, wf; detailed=true, tolerance=1e-6)
     end
 end
 
