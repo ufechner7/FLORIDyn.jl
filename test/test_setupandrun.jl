d1b8e4cb3974ccccc47c11bdfe57d7677db8ff65
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-08-15 19:05:46 +0200

    Cleanup

--- test/test_setupandrun.jl
+++ test/test_setupandrun.jl
@@ -103,31 +103,12 @@ end
     # create settings struct
     set = Settings(wind, sim, con)
     wf, wind, sim, con, floris = prepareSimulation(set, wind, con, floridyn, floris, ta, sim)
-    wf.dep = findTurbineGroups(wf, floridyn)
-    wf.intOPs = interpolateOPs(wf)
     sim.n_sim_steps = 2
     wf1 = convert_wf_dict2windfarm(wf_dict)
-    wf_old = deepcopy(wf)
     tmpM, wf = setUpTmpWFAndRun(set, wf1, floris, wind)
     tmpM_ref = [0, 9.094956525547340e-02, 0.1184213787906785, 0, 9.094956525547351e-02,
                 0.1184213787906786, 0, 9.094956525547340e-02, 1.010889141095400e-01]
     @test all(tmpM[:,2] .≈ tmpM_ref)
-    # compare_windFarms(wf1, wf_old; detailed=true)
 end
-
-@testset "setUpTmpWFAndRun_vs_matlab_ii" begin
-    global wf, wf_ref, floris
-    settings_file = "data/2021_9T_Data.yaml"
-    # get the settings for the wind field, simulator and controller
-    wind, sim, con, floris, floridyn, ta = setup(settings_file)
-    # create settings struct
-    set = Settings(wind, sim, con)
-    wf, wind, sim, con, floris = prepareSimulation(set, wind, con, floridyn, floris, ta, sim)
-    sim.n_sim_steps = 1
-    wf_ref = convert_wf_dict2windfarm(wf_dict_ref)
-    tmpM, wf = setUpTmpWFAndRun(set, wf_ref, floris, wind)
-    @test_broken compare_windFarms(wf1, wf_old; detailed=false)
-end
-
 
 nothing
