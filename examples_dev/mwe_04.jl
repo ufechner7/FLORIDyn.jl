ec8d0c7deb98bc081c810acd9ef3d463267c56d9
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-08-16 21:39:49 +0200

    Add comment

--- examples_dev/mwe_04.jl
+++ examples_dev/mwe_04.jl
@@ -43,3 +43,4 @@ nothing
 # 219.869886 seconds (61.49 M allocations: 5.326 GiB, 0.26% gc time, 6.08% compilation time)
 
 # 1.203399 seconds (54.85 M allocations: 5.027 GiB, 33.68% gc time)
+# 1.125934 seconds (54.85 M allocations: 5.027 GiB, 30.31% gc time)
