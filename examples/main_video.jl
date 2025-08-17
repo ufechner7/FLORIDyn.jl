d5f24604a311d164f4e95630c750c90a518dea34
Uwe Fechner <fechner@aenarete.eu>
2025-08-17 11:13:48 +0200

    Fix example

--- examples/main_video.jl
+++ examples/main_video.jl
@@ -12,8 +12,10 @@ vis_file      = "data/vis_default.yaml"
 
 vis = Vis(vis_file)
 vis.show_plots = true  # Enable/disable showing plots during simulation
-if @isdefined plt
+if (@isdefined plt) && !isnothing(plt)
     plt.ion()
+else
+    plt = nothing
 end
 
 # Automatic parallel/threading setup
@@ -28,7 +30,6 @@ wind, sim, con, floris, floridyn, ta = setup(settings_file)
 set = Settings(wind, sim, con, Threads.nthreads() > 1, Threads.nthreads() > 1)
 
 wf, wind, sim, con, floris = prepareSimulation(set, wind, con, floridyn, floris, ta, sim)
-sim.n_sim_steps = 5
 toc()
 
 vis.online = true
@@ -37,7 +38,4 @@ cleanup_video_folder()
 @time wf, md, mi = run_floridyn(plt, set, wf, wind, sim, con, vis, floridyn, floris)
 nothing
 
-# With Bumper:
-#  88.343513 seconds (10.41 G allocations: 825.498 GiB, 29.10% gc time, 2.40% compilation time)
-# Without Bumper:
-#  85.383338 seconds (11.82 G allocations: 873.189 GiB, 29.14% gc time, 1 lock conflict, 1.41% compilation time)
+#  66.826362 seconds (8.18 G allocations: 752.342 GiB, 30.41% gc time, 8 lock conflicts)
