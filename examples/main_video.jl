6a34a133d87631dc88c5d1173b13cf8a78582b32
Uwe Fechner <fechner@aenarete.eu>
2025-08-17 09:54:03 +0200

    Revert "Fast, but with artifacts"
    This reverts commit b8c30e49c685548cc22f77d08d51bbf5bdd5b2bc.

--- examples/main_video.jl
+++ examples/main_video.jl
@@ -12,10 +12,8 @@ vis_file      = "data/vis_default.yaml"
 
 vis = Vis(vis_file)
 vis.show_plots = true  # Enable/disable showing plots during simulation
-if (@isdefined plt) && ! isnothing(plt)
+if @isdefined plt
     plt.ion()
-else
-    plt = nothing
 end
 
 # Automatic parallel/threading setup
@@ -30,6 +28,7 @@ wind, sim, con, floris, floridyn, ta = setup(settings_file)
 set = Settings(wind, sim, con, Threads.nthreads() > 1, Threads.nthreads() > 1)
 
 wf, wind, sim, con, floris = prepareSimulation(set, wind, con, floridyn, floris, ta, sim)
+sim.n_sim_steps = 5
 toc()
 
 vis.online = true
