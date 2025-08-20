bf334ec0e36f6ae8c83b5876bd1b77f871fd3f4b
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-08-20 12:54:40 +0200

    Revert "Update test/test_visualisation.jl"
    This reverts commit 0cd41c40931b56ef96315c966f35c3ce2674b26c.

--- test/test_visualisation.jl
+++ test/test_visualisation.jl
@@ -286,8 +286,7 @@ if ! isinteractive()
                 # Calculate expected grid dimensions
                 # Grid points = (max - min) / resolution + 1
                 expected_x_points = Int(round((custom_vis.field_limits_max[1] - custom_vis.field_limits_min[1]) / custom_vis.field_resolution)) + 1
-                expected_x_points = expected_grid_points(custom_vis.field_limits_min[1], custom_vis.field_limits_max[1], custom_vis.field_resolution)
-                expected_y_points = expected_grid_points(custom_vis.field_limits_min[2], custom_vis.field_limits_max[2], custom_vis.field_resolution)
+                expected_y_points = Int(round((custom_vis.field_limits_max[2] - custom_vis.field_limits_min[2]) / custom_vis.field_resolution)) + 1
                 
                 # Validate grid dimensions match vis parameters
                 @test size(X, 2) == expected_x_points  # X grid has expected number of columns
