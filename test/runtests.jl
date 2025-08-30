cc5f47a62b55608e551265f494c58606ed1f0412
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-08-30 18:22:59 +0200

    Integrate test

--- test/runtests.jl
+++ test/runtests.jl
@@ -58,6 +58,7 @@ all_test_files = [
     "test_correction.jl",
     "test_direction_none.jl",
     "test_direction_influence.jl",
+    "test_velocity_influence.jl",
     "test_controller.jl",
     "test_settings.jl",
     "test_high_res_time.jl",
