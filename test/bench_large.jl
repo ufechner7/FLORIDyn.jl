261aa2a705d030a0d62ad6a14a6f3b4731c7b561
Uwe Fechner <fechner@aenarete.eu>
2025-08-30 21:44:14 +0200

    Add benchmark

---
+++ test/bench_large.jl
@@ -0,0 +1,35 @@
+# Copyright (c) 2025 Marcus Becker, Uwe Fechner
+# SPDX-License-Identifier: BSD-3-Clause
+
+# MainFLORIDyn Center-Line model
+# Improved FLORIDyn approach over the gaussian FLORIDyn model
+
+# Minimal example of how to run a simulation using FLORIDyn.jl
+using FLORIDyn, TerminalPager, DistributedNext, BenchmarkTools
+
+settings_file = "data/2021_54T_NordseeOne.yaml"
+vis_file      = "data/vis_54T.yaml"
+
+# Load vis settings from YAML file
+vis = Vis(vis_file)
+vis.show_plots = false
+if (@isdefined plt) && !isnothing(plt)
+    plt.ion()
+else
+    plt = nothing
+end
+
+# get the settings for the wind field, simulator and controller
+wind, sim, con, floris, floridyn, ta = setup(settings_file)
+
+# create settings struct with automatic parallel/threading detection
+set = Settings(wind, sim, con, Threads.nthreads() > 1, Threads.nthreads() > 1)
+
+wf, wind, sim, con, floris = prepareSimulation(set, wind, con, floridyn, floris, ta, sim)
+
+# Run initial conditions
+wf = initSimulation(wf, sim)
+
+vis.online = false
+@btime run_floridyn(plt, set, wf, wind, sim, con, vis, floridyn, floris)
+nothing
