aa0b32eb8662f97b57ff42c9eaae888a4608fd50
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-09-01 13:40:38 +0200

    Add labels

--- examples_dev/main_power_plot.jl
+++ examples_dev/main_power_plot.jl
@@ -40,24 +40,25 @@ wf = initSimulation(wf, sim)
 
 vis.online = false
 @time wf, md, mi = run_floridyn(plt, set, wf, wind, sim, con, vis, floridyn, floris)
-# plot_measurements(wf, md, vis; separated=false, msr=VelReduction, plt, pltctrl)
+plot_measurements(wf, md, vis; separated=false, msr=VelReduction, plt, pltctrl)
 
 data_column = "ForeignReduction"
-title = "Velocity Reduction"
 ylabel = "Rel. Wind Speed [%]"
-msr_name = "msr_velocity_reduction"
 
 times, plot_data, turbine_labels, subplot_labels = FLORIDyn.prepare_large_plot_inputs(wf, md, data_column, ylabel; simple=true)
-# plot_x(times, plot_data...; ylabels=turbine_labels, labels=subplot_labels,
-#         fig=title, xlabel="rel_time [s]", ysize=9, bottom=0.02, pltctrl, legend_size=6, loc="center left")
 nT = wf.nT
 power_sum = zeros(length(times))
 for iT in 1:nT
+    local rel_power, rel_speed
     rel_speed = plot_data[1][iT] ./ 100
     rel_power = rel_speed .^3
     power_sum .+= rel_power
 end
 power_sum ./= nT
 
-plt.plot(times,power_sum)
+plt.figure()
+plt.plot(times,power_sum .* 100)
+plt.title("Relative Power Output")
+plt.xlabel("Time [s]")
+plt.ylabel("Rel. Power Output [%]")
 plt.grid(true)
