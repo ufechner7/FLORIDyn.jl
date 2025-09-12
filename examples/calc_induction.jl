cb58cecf066f7311b348339417e7d32f39cb155b
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-09-12 20:59:46 +0200

    Some progress

--- examples/calc_induction.jl
+++ examples/calc_induction.jl
@@ -1,4 +1,16 @@
-# Calculate axial induction factor
+# Copyright (c) 2025 Marcus Becker, Uwe Fechner
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Calculate axial induction factor, and calculate the demand
+
+using FLORIDyn, ControlPlots, YAML
+
+settings_file = get_default_project()[2]
+
+# get the settings for the wind field, simulator, controller and turbine array
+wind, sim, con, floris, floridyn, ta = setup(settings_file)
+
+
 time_step = 4.0 # seconds
 
 function calc_axial_induction(turbine_group, time)
@@ -7,5 +19,11 @@ function calc_axial_induction(turbine_group, time)
 end
 
 function calc_axial_induction(turbine, time)
+    turbine_group = turbine_group(ta, turbine)
     return 1/3  # Constant value for all turbines
+end
+
+function calc_demand(time)
+    # Example: simple constant demand
+    return 1.0  # Constant value
 end
