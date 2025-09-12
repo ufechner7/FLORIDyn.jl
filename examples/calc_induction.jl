01c548cf431d4fb427aa6ade325d0f907cfb22a8
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-09-12 21:25:07 +0200

    Some progress

--- examples/calc_induction.jl
+++ examples/calc_induction.jl
@@ -24,6 +24,44 @@ function calc_axial_induction(turbine, time)
     return 1/3  # Constant value for all turbines
 end
 
+function calc_cp(induction)
+    return 4 * induction * (1 - induction)^2
+end
+function calc_induction(cp)
+    # Solve cubic equation: 4a^3 - 8a^2 + 4a - cp = 0
+    # Using Cardano's method for cubic equations
+    a = 0.0
+    b = -2.0
+    c = 1.0
+    d = -cp / 4.0
+
+    Δ0 = b^2 - 3*a*c
+    Δ1 = 2*b^3 - 9*a*b*c + 27*a^2*d
+
+    C = ((Δ1 + sqrt(Δ1^2 - 4*Δ0^3)) / 2)^(1/3)
+
+    if C == 0
+        C = ((Δ1 - sqrt(Δ1^2 - 4*Δ0^3)) / 2)^(1/3)
+    end
+
+    ξ = exp(2π*im/3)  # Complex cube root of unity
+
+    roots = [
+        -(1/(3*a)) * (b + C + Δ0 / C),
+        -(1/(3*a)) * (b + ξ*C + Δ0 / (ξ*C)),
+        -(1/(3*a)) * (b + ξ^2*C + Δ0 / (ξ^2*C))
+    ]
+
+    # Filter real roots in the range [0, 1]
+    real_roots = filter(r -> isreal(r) && r >= 0 && r <= 1, roots)
+    
+    if length(real_roots) == 0
+        error("No valid real root found for cp=$cp")
+    end
+
+    return real(real_roots[1])  # Return the first valid real root
+end
+
 function calc_demand(time)
     # Example: linearly increasing demand from 0.5 to 1.0 over the simulation time
     initial_demand = 0.5
