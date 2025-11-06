d3b9995055ba6fc94f2d054082c05c919d60984d
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-11-06 20:13:44 +0100

    Some progress for 12 TGs

--- examples/mpc.jl
+++ examples/mpc.jl
@@ -21,11 +21,11 @@ data_file               = "data/mpc_result.jld2"
 error_file              = "data/mpc_error.jld2"
 data_file_group_control = "data/mpc_result_group_control"
 
-GROUPS = 8 # must be 4, 8 or 12
+GROUPS = 12 # must be 4, 8 or 12
 GROUP_CONTROL = true  # if false, use 3-parameter control for all turbines; if true, use 10-parameter group control
 MAX_ID_SCALING = 3.0
 SIMULATE = true      # if false, load cached results if available
-MAX_STEPS = 200      # maximum number black-box evaluations for NOMAD optimizer
+MAX_STEPS = 20      # maximum number black-box evaluations for NOMAD optimizer
 USE_TGC = false
 USE_STEP = false
 USE_FEED_FORWARD = true # if false, use constant induction (no feed-forward)
@@ -614,7 +614,7 @@ else
             x0 = [1.60271, 1.977, 1.5607, 1.36922, 1.309, 0.33, 0.6913, 2.0351]
         elseif GROUPS == 12
             # 5 global + 11 group parameters (last group calculated from constraint)
-            x0 = [1.4, 1.7, 1.35, 1.28, 1.35, 0.010165, 0.029492, 0.019167, 2.88922, 0.0181, 2.115, 1.38954, 0.8827, 1.14561, 1.00147, 0.9951]
+            x0 = [1.4, 1.7, 1.35, 1.28, 1.35, 0.010165, 0.029492, 0.019167, 1.88922, 0.0181, 2.115, 1.38954, 1.8827, 1.14561, 1.00147, 0.9951]
         else
             # Generic initial guess for other group counts
             x0 = vcat([1.5, 1.5, 1.5, 1.5, 1.5], fill(1.0, GROUPS - 1))
