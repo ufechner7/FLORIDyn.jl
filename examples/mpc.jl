35756ca525a0dc2e2df1d40799156e73fdfc5ea7
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-11-06 20:03:04 +0100

    Improve 8TG model

--- examples/mpc.jl
+++ examples/mpc.jl
@@ -25,7 +25,7 @@ GROUPS = 8 # must be 4, 8 or 12
 GROUP_CONTROL = true  # if false, use 3-parameter control for all turbines; if true, use 10-parameter group control
 MAX_ID_SCALING = 3.0
 SIMULATE = true      # if false, load cached results if available
-MAX_STEPS = 100      # maximum number black-box evaluations for NOMAD optimizer
+MAX_STEPS = 200      # maximum number black-box evaluations for NOMAD optimizer
 USE_TGC = false
 USE_STEP = false
 USE_FEED_FORWARD = true # if false, use constant induction (no feed-forward)
@@ -609,7 +609,7 @@ else
     if GROUP_CONTROL
         # Create initial guess: 5 global parameters + (GROUPS-1) group parameters
         if GROUPS == 8
-            x0 = [1.327, 1.368, 1.38, 1.248, 1.318, 0.0, 0.0, 1.92, 1.91, 1.95, 0.9, 0.0]
+            x0 = [1.325, 1.472, 1.33, 1.292, 1.32, 0.0, -0.0, 1.91, 1.92, 1.99, 0.87, 0.0]
         elseif GROUPS == 4
             x0 = [1.60271, 1.977, 1.5607, 1.36922, 1.309, 0.33, 0.6913, 2.0351]
         elseif GROUPS == 12
