036537b7fc3760489d055281b354e2962d3ac1ec
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-11-07 10:32:06 +0100

    Default now 8

--- examples/mpc.jl
+++ examples/mpc.jl
@@ -21,11 +21,11 @@ data_file               = "data/mpc_result.jld2"
 error_file              = "data/mpc_error.jld2"
 data_file_group_control = "data/mpc_result_group_control"
 
-GROUPS = 4 # must be 4, 8 or 12
+GROUPS = 8 # must be 4, 8 or 12
 GROUP_CONTROL = true  # if false, use 3-parameter control for all turbines; if true, use 10-parameter group control
 MAX_ID_SCALING = 3.0
 SIMULATE = true      # if false, load cached results if available
-MAX_STEPS = 200        # maximum number black-box evaluations for NOMAD optimizer
+MAX_STEPS = 1        # maximum number black-box evaluations for NOMAD optimizer
 USE_TGC = false
 USE_STEP = false
 USE_FEED_FORWARD = true # if false, use constant induction (no feed-forward)
