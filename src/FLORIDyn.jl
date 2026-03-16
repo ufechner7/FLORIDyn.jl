fc47bf2df7c33841fba680f52d1b8bdd57f8be0f
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-03-16 17:04:07 +0100

    Fix warning

--- src/FLORIDyn.jl
+++ src/FLORIDyn.jl
@@ -669,12 +669,11 @@ end
 @setup_workload begin
     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
     # precompile file and potentially make loading faster.
-    path = joinpath(PACKAGE_ROOT, "data")
     vis = Vis(online=false)
     @compile_workload begin
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
-        settings_file = joinpath(path, "2021_9T_Data.yaml")
+        settings_file = joinpath(PACKAGE_ROOT, "data", "2021_9T_Data.yaml")
         setup_fn = getfield(FLORIDyn, :setup)
         prepare_simulation_fn = getfield(FLORIDyn, :prepareSimulation)
         init_simulation_fn = getfield(FLORIDyn, :initSimulation)
